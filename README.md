f21ffe07900ee084aec82b3c72dde18313231852
Codebuilder2022 <kumarankaushik@gmail.com>
2021-05-17 23:28:44 +0400

    ✨ Mailer SET

---
+++ README.md
@@ -0,0 +1,28 @@
+<p align="center">
+  <img src="https://github.com/Codebuilder2022/Codebuilder2022/blob/06e882617529167abd76823af3dd4217ddb127fe/kkbgr.png" />
+</p>
+
+<p align = "center">
+  <a href = "https://twitter.com/KashKS20" target="_blank"><img src = "https://img.shields.io/twitter/url?color=Blue&label=KashKS&logo=Twitter&logoColor=White&style=social&url=https%3A%2F%2Ftwitter.com%2FKashKS20" /></a>
+  <a href = "https://www.linkedin.com/in/kaushikkums/" target = "_blank"><img src = "https://img.shields.io/badge/-KashKS-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/kaushikkums/" /></a>
+  <img src="https://badges.pufler.dev/Visits/Codebuilder2022/Codebuilder2022?style=flat-square&color=orange" />
+</p>
+<br><br>
+
+ <img align="right" src="https://github-readme-streak-stats.herokuapp.com/?user=Codebuilder2022&theme=nightowl&border_radius=20" />
+ 
+ #### About Me ✨
+ 
+- 🌱 I’m currently preparing for my ACE certification.
+- 💬 Ask me about Web Stuff and Cloud Stuff.
+- 🔭 I’m currently working on Web Based Applications.
+- 📫 Reach me at <a href = "http://skaushik.xyz/" target = "_blank">hitmeup@kashKS</a>
+
+
+<br><br><br>
+<p align = "center">
+  <img align="center" src="https://github-readme-stats.vercel.app/api?username=Codebuilder2022&theme=nightowl&count_private=true&include_all_commits=true&border_radius=20&show_icons=true&custom_title=%20Kaushik%27s%20GitHub%20Stats%20" width="460" />
+</p>
+<p align = "center">
+   <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Codebuilder2022&theme=nightowl&layout=compact&langs_count=12&border_radius=20&count_private=true&include_all_commits=true&custom_title=%20Most%20Used%20Languages%20By%20Kaushik" />
+</p>
---
+++ api/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+require("dotenv").config({ path: "./.env" });
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+const port = process.env.PORT || 3000;
+
+app.post("/send", (req, res) => {
+  const output = `
+    <p>Scylla here, Someone is trying to contact you!</p>
+    <h3>Contact Details:</h3>
+    <ul type ="square">  
+      <li>Name: ${req.body.name}</li>
+      <li>Subject: ${req.body.subject}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message Fetched:</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.PASSWORD, // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  let mailOptions = {
+    name: req.body.name,
+    from: req.body.email, // sender address
+    to: "kums2kaushik@gmail.com", // list of receivers
+    subject: `Scylla here! ${req.body.email} is trying to get in touch with you.`,
+    text: req.body.message,
+    html: output, // plain text body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(400);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.status(201).send({ msg: "Email has been sent" });
+  });
+});
+app.listen(port, () => {
+  console.log(`Wakey Wakey kk, Server is up and running @ ${port}`);
+});
